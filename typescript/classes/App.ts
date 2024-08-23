9f7131e1db79b7798bcec98d55a51538acbb5b6f
asom1989 <asom.1989@yahoo.com>
2024-08-23 14:50:57 +0200

    add players

--- typescript/classes/App.ts
+++ typescript/classes/App.ts
@@ -1,14 +1,28 @@
+import readlineSync from "readline-sync";
 import Board from "./Board.js";
+import Player from "./Player.js";
 
 export default class App {
   board: Board;
+  playerX!: Player;
+  playerO!: Player;
 
   constructor() {
     this.board = new Board();
-    this.start();
+    this.createPlayers();
+    this.startGame();
   }
 
-  start() {
+  createPlayers() {
+    console.clear();
+    console.log("Connect 4 Game\n");
+    const playerXName = readlineSync.question("Spelare X namn: ") || "X";
+    const playerOName = readlineSync.question("Spelare O namn: ") || "O";
+    this.playerX = new Player(playerXName, "X");
+    this.playerO = new Player(playerOName, "O");
+  }
+
+  startGame() {
     this.board.render();
   }
 }
