1e27009e8258aedc7ab80219faea1b3d44efe993
asom1989 <asom.1989@yahoo.com>
2024-08-23 13:44:09 +0200

    build a game table

--- typescript/classes/Board.ts
+++ typescript/classes/Board.ts
@@ -1,1 +1,27 @@
-export default class Board {}
+export default class Board {
+  matrix: string[][];
+
+  constructor() {
+    this.matrix = [1, 2, 3, 4, 5, 6].map(() =>
+      [1, 2, 3, 4, 5, 6, 7].map(() => " ")
+    );
+  }
+
+  render() {
+    let line = "\n" + "+---".repeat(7) + "+" + "\n";
+    let numColumns = 7;
+    let columnNumbers = Array.from(
+      { length: numColumns },
+      (_, i) => `  ${i + 1}`
+    ).join(" ");
+
+    console.log(
+      columnNumbers +
+        line +
+        this.matrix
+          .map((row) => row.map((column) => `| ${column} `).join("") + "|")
+          .join(line) +
+        line
+    );
+  }
+}
